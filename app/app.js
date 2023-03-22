606e57508ea8cd634b340b8db6f93275ad800f99
EC2 Default User <ec2-user@ip-172-31-32-30.ap-northeast-1.compute.internal>
2023-03-22 13:08:30 +0000

    Initial Commit

--- app/app.js
+++ app/app.js
@@ -1,1 +1,22 @@
-alert('Hello World');
+let hello = 'Hello World';
+alert(hello);
+
+// 整数を代入する
+let int1 = 1;
+
+// 負数を代入する
+let int2 = -10;
+
+// 小数点を代入する
+let float1 = 3.14;
+
+// 文字列を代入する
+let str1 = 'JavaScriptを覚えよう';
+
+//文字列の結合は+ 記号を用いる
+alert('Hello' + 'World');
+
+//結合の応用
+let str2 = 'Hello';
+let str3 = 'World!!';
+alert(str1 + str2);
